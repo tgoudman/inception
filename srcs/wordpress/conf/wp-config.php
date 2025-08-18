f3b644ae62d099d75a11b645bd9c15be0d20af62
Thibault Goudmand <tgoudman@c1r7p9.42lehavre.fr>
2025-08-18 16:55:00 +0200

    monday

--- srcs/wordpress/conf/wp-config.php
+++ srcs/wordpress/conf/wp-config.php
@@ -1,18 +1,4 @@
 <?php
-// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
-/** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'BDDtgoudman' );
-/** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'tgoudman' );
-/** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'millefeuille' );
-/** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
-/** Jeu de caractères à utiliser pour la base de données lors de la création des tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
-/** Type de collation de la base de données. Ne modifiez pas si vous ne savez pas. */
-define( 'DB_COLLATE', '' );
-/** Clés d’authentification et salage - changez-les pour sécuriser votre site */
 define('AUTH_KEY',         'mets une phrase unique ici');
 define('SECURE_AUTH_KEY',  'mets une phrase unique ici');
 define('LOGGED_IN_KEY',    'mets une phrase unique ici');
